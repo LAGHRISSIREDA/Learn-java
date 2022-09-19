9a0c8017cb6ef0635cd607e94aa855cfb8febfcd
REDA LAGHRISSI <laghrissi.laghrissi36@gmail.com>
2022-09-19 21:29:44 +0100

    Check if Number Odd or even

--- src/com/reda/Main.java
+++ src/com/reda/Main.java
@@ -1,12 +1,22 @@
 package com.reda;
 
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        String rejouer = "non";
-        int number;
-        System.out.println("Heelooooo");
+        String replay = "oui";
+        Scanner scan = new Scanner(System.in);
+        while (replay.contains("oui")){
+            System.out.println("Donner Un numero");
+            int number =Integer.parseInt(scan.nextLine());
+            //String result = ;
+            System.out.println((number % 2 == 0)?"le nombre est paire": "le nombre est impair");
+           //12 scan.close();
+            System.out.println("Voulez-vous rejouer ? si oui TAPER 'oui' sinon taper n'importe quel boutton .");
+            replay = scan.nextLine();
+        }
+        System.out.println("Aurevoir !!!");
     }
 
 }
